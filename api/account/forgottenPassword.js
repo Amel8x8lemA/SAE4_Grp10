d50622e639ddebe7dbec10b08a9bdcc25da3d93c
Amel8x8lemA <ameliendenis72@gmail.com>
2024-04-03 17:12:46 +0200

    fix FS02 usurpation de compte

--- api/account/forgottenPassword.js
+++ api/account/forgottenPassword.js
@@ -1,37 +1,37 @@
 import express from 'express';
 const router = express.Router();
-import {pool, sendEmail, forgotten_passcodes} from '../../server.js';
+import { pool, sendEmail, forgotten_passcodes } from '../../server.js';
 
 router.post('', async (req, res) => {
-  const {email} = req.body;
+  const { email } = req.body;
 
-  // Check if the email already exists
-  const [exists] = await pool.query(
-    'SELECT * FROM user WHERE email = ?',
-    [email],
-    (err, results) => {
-      if (err) {
-        console.error('Impossible de récupérer le mail :', err);
-        res.status(500).json({error: 'Impossible de récupérer le mail'});
-        return;
-      }
-    }
-  );
-
-  if (exists.length === 0) {
-    res.status(409).json({error: 'Email non utilisée'});
+  // Si un utilisateur est connecté, vérifiez si l'email entré est celui de la session en cours
+  if (req.session.isLoggedIn && !(req.session.email === email)) {
+    // L'utilisateur est connecté et l'email entré n'est pas celui de la session en cours
+    res.status(408).json({ error: "L'adresse email entrée n'est pas la même" });
     return;
   }
 
-  // Email found, send the passcode
+  // Vérifiez si l'email existe déjà dans la base de données
+  const [exists] = await pool.query('SELECT * FROM user WHERE email = ?', [email]);
+
+  if (exists.length === 0) {
+    // L'email n'existe pas dans la base de données
+    res.status(409).json({ error: 'Email non utilisée' });
+    return;
+  }
+
+  // L'email existe dans la base de données, envoyez le code de vérification
   console.log('Sending forgotten password passcode email to ' + email + '...');
   const passcode = Math.floor(Math.random() * 1000000);
 
-  //add the passcode to the forgotten_passcodes object
+  // Ajouter le code de vérification à l'objet forgotten_passcodes
   forgotten_passcodes[email] = passcode;
 
+  // Stockez l'email dans la session
   req.session.email = email;
 
+  // Envoyer l'email avec le code de vérification
   await sendEmail(
     email,
     "Vérification de réinitialisation de votre mot de passe de l'ADIIL",
@@ -39,7 +39,7 @@ router.post('', async (req, res) => {
     'réinitialisation de mot de passe'
   );
 
-  res.status(200).json({success: true});
+  res.status(200).json({ success: true });
 });
 
 export default router;
